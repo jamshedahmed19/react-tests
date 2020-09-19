2649c69701586d1707ae1ce7d585e87383a46561
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2020-09-19 21:22:29 +0500

    build

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Jamshed/i);
   expect(linkElement).toBeInTheDocument();
 });
